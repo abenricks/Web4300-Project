5d4af625c8f328167fdfc0447ba2bd537015f897
abenricks <abenricks@gmail.com>
2022-11-11 16:18:01 -0500

    web-app

---
+++ web-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*Change name of webpage*/}
+    <div class="menuTitle">Webpage Title?</div>
+
+    {/*Links need to be added or open components through router maybe*/}
+    <div class="menu">
+        <div class="menuItem">Item1ex</div>
+        <div class="menuItem">Item2ex</div>
+        <div class="menuItem">Item3ex</div>
+    </div>
+
+    {/*Intro banner and text abt the site*/}
+    <div class="intro">
+        Etc etc intro...
+    </div>
+
+    <div id="topArticle"></div>
+    </div>
+  );
+}
+
+export default App;
