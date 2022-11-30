8a61948a1e4d7723e2c767755076635927ef42e9
Pukonala S <ss38931@uga.edu>
2022-11-29 23:15:47 -0500

    update and delete are working

--- web-app/src/components/singlePost/SinglePost.js
+++ web-app/src/components/singlePost/SinglePost.js
@@ -4,19 +4,45 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import "./singlePost.css";
+import {Context} from "../../context/Context";
+import { useContext } from "react";
 
 export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const {user} = useContext(Context);
+  const [update, setUpdate] = useState(false);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
 
 useEffect (() => {
     const getPost = async () => {
       const res = await axios.get("/posts/" + path);
       setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
     };
     getPost();
   }, [path]);
+
+  const handleDelete = async()=>{
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+        data: {username:user.username},
+      });
+      window.location.replace("/");
+    } catch (err) {}
+  };
+
+  const handleUpdate = async ()=>{
+    try {
+      await axios.put(`/posts/${post._id}`, {
+        username:user.username, title:title, desc:desc,
+      });
+      setUpdate(false)
+    } catch (err) {}
+  }
 
   return (
     <div className="singlePost">
@@ -27,14 +53,17 @@ useEffect (() => {
           src={post.photo}
           alt=""
         />
-        )}
-        <h1 className="singlePostTitle">
-          {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
-        </h1>
+        )}{
+          update ? <input type="text" value={title} className="singlePostTitleInput" autoFocus onChange={(e)=>setTitle(e.target.value)}/> : (
+            <h1 className="singlePostTitle">
+              {post.title}
+                <div className="singlePostEdit">
+                   <i className="singlePostIcon far fa-edit" onClick={()=>setUpdate(true)}></i>
+                   <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+               </div>
+            </h1>
+          )
+        }
         <div className="singlePostInfo">
           <span>
             Author:
@@ -44,9 +73,16 @@ useEffect (() => {
           </span>
           <span>{new Date(post.createdAt).toDateString()}</span>
         </div>
-        <p className="singlePostDesc">
+        {update ? (
+          <textarea className="singlePostDescInput" value={desc} onChange={(e)=>setDesc(e.target.value)}/> 
+        )  : (
+          <p className="singlePostDesc">
           {post.desc}
-        </p>
+          </p>
+        )}
+        {update && (
+          <button className="singlePostButton" onClick={handleUpdate}>Update</button>
+        )}
       </div>
     </div>
   );
